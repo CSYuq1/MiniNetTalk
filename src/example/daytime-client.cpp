26205ea2c6373c4cd6ebe2d0ad1bf3ac7a030896
yanyuqi_fedora <yanyuqi2004@gmail.com>
2026-01-19 15:11:41 +0800

    null

---
+++ src/example/daytime-client.cpp
@@ -0,0 +1,42 @@
+#include<print>
+#include"asio.hpp"
+
+int main(int argc, char* argv[]) {
+    asio::io_context io_context;
+    asio::error_code ec;
+
+    asio::ip::tcp::resolver resolver(io_context);
+    auto                    endpoints = resolver.resolve("time-c.timefreq.bldrdoc.gov", "13");
+    if (!endpoints.empty()) {
+        std::println("get ip success!");
+        uint32_t i = 1;
+        for (auto&res: endpoints) {
+            std::print(" ip{}: {}\n", i++, res.endpoint().address().to_string());
+        }
+    }
+
+    asio::ip::tcp::socket socket{io_context};
+    auto                  endpoint = asio::connect(socket, endpoints, ec);
+    if (!ec)
+        std::println("connect success!");
+
+
+    asio::streambuf buffer{1024};
+    asio::async_read(socket, buffer,
+                     [&](const asio::error_code&error_code, std::size_t bytes_transferred ) {
+                         std::println("buffer.size() = {}", buffer.size());
+                         std::println("ec = {}", error_code ? error_code.message() : "success");
+
+
+                         std::string data(
+                                          asio::buffers_begin(buffer.data()),
+                                          asio::buffers_begin(buffer.data()) + bytes_transferred
+                                         );
+                         std::println("recv data: {} ", data);
+                         buffer.consume(bytes_transferred);
+                     });
+
+    //std::print("hello");
+    io_context.run();
+    return 0;
+}
