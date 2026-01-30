506d303a95c7a8f36da8c4e7dcbbebcb6890b41c
yuqi_pc <yanyuqi2004@gmail.com>
2026-01-31 02:41:51 +0800

    null

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,6 +1,7 @@
 //
 // Created by yuqi on 2026/1/24.
 //
+#include<print>
 #include"boost/asio.hpp"
 #include"boost/beast.hpp"
 
@@ -8,14 +9,23 @@
 namespace asio = boost::asio;
 namespace beast = boost::beast;
 
-using tcp=boost::asio::ip::tcp;
+using tcp        = boost::asio::ip::tcp;
+using udp        = boost::asio::ip::udp;
+using error_code = boost::system::error_code;
 
-namespace minitalk
+namespace mini_talk
 {
+	namespace server
+	{
+
+	}
 }
-int main(int argc , char *argv[])
+
+int main(int argc, char* argv[])
 {
-	asio::io_context io_c;
+	asio::io_context io_context;
+	error_code       ec;
+	tcp::acceptor    acceptor(io_context, tcp::endpoint(tcp::v6(), mini_talk::server::listen_port));
 
 	return 0;
 }
